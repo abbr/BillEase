fe95473338552eae4145c8f78ff6729e8b1c0942
abbr <wenfred@gmail.com>
2018-01-15 14:40:04 -0800

    Avoid using ADO.NET Data Services due to lack flexibility.
    Use CSOM exclusively to manipulate SP.

--- Invoice Run/Program.cs
+++ Invoice Run/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Invoice_Run.BillingSite;
 using Microsoft.SharePoint.Client;
 using System.Diagnostics;
 using NDesk.Options;
@@ -38,62 +37,8 @@ namespace Invoice_Run
                 };
                 List<String> extraArgs = options.Parse(args);
                 ServicePointManager.ServerCertificateValidationCallback = MyCertHandler;
-                var dc = new BillingsDataContext(new Uri(extraArgs[0] + "/_vti_bin/ListData.svc"));
-                //dc.IgnoreMissingProperties = true;
-                dc.Credentials = System.Net.CredentialCache.DefaultCredentials;
-                var cycleDt = DateTime.Now.AddMonths(cycleMonthOffset);
-                var consumptions = from consumption in dc.Consumptions
-                                   where consumption.Cycle.Value.Month == cycleDt.Month
-                                   && consumption.Cycle.Value.Year == cycleDt.Year
-                                   select consumption;
-                foreach (var consumption in consumptions)
-                {
-                    consumption.Rate = dc.Rates.Where(r => r.Id == consumption.RateId).First();
-                    consumption.Organization = dc.Organizations.Where(o => o.Id == consumption.OrganizationId).First();
-                    var existingCiCnt = dc.Charges.Where(c => c.ConsumptionRefId == consumption.Id).Count();
-                    if (existingCiCnt > 0) continue;
-                    var ci = new ChargesItem();
-                    ci.Organization = consumption.Organization.Name;
-                    ci.Title = consumption.Title;
-                    ci.Cycle = consumption.Cycle;
-                    ci.UnitPrice = consumption.Rate.UnitPrice;
-                    ci.Denominator = consumption.Rate.Denominator;
-                    ci.UOM = consumption.Rate.UOMValue;
-                    ci.Quantity = consumption.Quantity;
-                    ci.ConsumptionRefId = consumption.Id;
-                    if (consumption.Amount.HasValue)
-                        ci.Amount = consumption.Amount.Value;
-                    else if (consumption.Quantity.HasValue
-                        && consumption.Rate.Denominator.HasValue
-                        && consumption.Rate.UnitPrice.HasValue
-                        && consumption.Rate.Denominator.Value > 0)
-                        ci.Amount = consumption.Rate.UnitPrice * Math.Ceiling(consumption.Quantity.Value / consumption.Rate.Denominator.Value);
-                    if (ci.Amount.HasValue)
-                        dc.AddToCharges(ci);
-                    else
-                    {
-                        EventLog.WriteEntry(evtLogSrc, "Cannot calculate amount for consumption item #" + consumption.Id, EventLogEntryType.Error);
-                    }
-                    foreach (KeyValuePair<string, List<string>> listColumnToCopy in listColumnsToCopy)
-                    {
-                        if (listColumnToCopy.Value.Count <= 0)
-                        {
-                            continue;
-                        }
-                        foreach (var columnNm in listColumnToCopy.Value)
-                        {
-                            var x = consumption.GetType().GetProperty(listColumnToCopy.Key).GetValue(consumption, null);
-                            var y = x.GetType().GetProperty(columnNm).GetValue(x, null);
-                            ci.GetType().GetProperty(columnNm).SetValue(ci, y, null);
-                        }
-
-                    }
-
-                }
-                dc.SaveChanges();
                 var cc = new ClientContext(extraArgs[0]);
                 cc.Credentials = System.Net.CredentialCache.DefaultCredentials;
-                var chargesLst = cc.Web.Lists.GetByTitle("Charges");
                 var query = new CamlQuery();
                 query.ViewXml = string.Format(@"
 <View><Query>
@@ -105,14 +50,9 @@ namespace Invoice_Run
    </Where>
 </Query></View>", DateTime.Now.AddMonths(cycleMonthOffset).ToString("yyyy-MM-01"));
 
-                var chargesLIC = chargesLst.GetItems(query);
-                cc.Load(chargesLIC, items => items.Include(
-                    item => item["Organization"]
-                    , item => item["HasUniqueRoleAssignments"]
-                    ));
                 var consumptionLst = cc.Web.Lists.GetByTitle("Consumptions");
                 var consumptionLIC = consumptionLst.GetItems(query);
-                cc.Load(consumptionLIC, items => items.Include(
+                cc.Load(consumptionLIC, items => items.IncludeWithDefaultProperties(
                     item => item["HasUniqueRoleAssignments"]
                     ));
                 cc.Load(cc.Web.RoleDefinitions);
@@ -121,25 +61,121 @@ namespace Invoice_Run
                 cc.ExecuteQuery();
                 var restReadRD = cc.Web.RoleDefinitions.GetByName("Restricted Read");
                 var readRD = cc.Web.RoleDefinitions.GetByName("Read");
-                foreach (var chargeLI in chargesLIC)
-                {
-                    if (!chargeLI.HasUniqueRoleAssignments)
-                    {
-                        chargeLI.BreakRoleInheritance(true, false);
-                    }
-                    foreach (var g in gc)
-                    {
-                        if (g.LoginName == (groupPrefix + chargeLI["Organization"].ToString()))
-                        {
-                            var rdb = new RoleDefinitionBindingCollection(cc);
-                            rdb.Add(restReadRD);
-                            chargeLI.RoleAssignments.Add(g, rdb);
-                            break;
-                        }
-                    }
-                }
                 foreach (var consumptionLI in consumptionLIC)
                 {
+                    // check if charges exists
+                    var chgItemQuery = new CamlQuery();
+                    chgItemQuery.ViewXml = string.Format(@"
+<View><Query>
+   <Where>
+      <Eq>
+         <FieldRef Name='Consumption_x0020_Ref' LookupId='TRUE' />
+         <Value Type='Lookup'>{0}</Value>
+      </Eq>
+   </Where>
+</Query></View>", consumptionLI["ID"]);
+                    var chgLst = cc.Web.Lists.GetByTitle("Charges");
+                    var chgLIC = chgLst.GetItems(chgItemQuery);
+                    cc.Load(chgLIC);
+                    cc.ExecuteQuery();
+                    if (chgLIC.Count > 0)
+                    {
+                        continue;
+                    }
+
+                    // get org item
+                    var orgItemQuery = new CamlQuery();
+                    orgItemQuery.ViewXml = string.Format(@"
+<View><Query>
+   <Where>
+      <Eq>
+         <FieldRef Name='ID' />
+         <Value Type='Counter'>{0}</Value>
+      </Eq>
+   </Where>
+</Query></View>", ((FieldLookupValue)consumptionLI["Organization"]).LookupId);
+                    var orgLst = cc.Web.Lists.GetByTitle("Organizations");
+                    var orgLIC = orgLst.GetItems(orgItemQuery);
+                    cc.Load(orgLIC);
+                    // get rate item
+                    var rateItemQuery = new CamlQuery();
+                    rateItemQuery.ViewXml = string.Format(@"<View><Query>
+   <Where>
+      <Eq>
+         <FieldRef Name='ID' />
+         <Value Type='Counter'>{0}</Value>
+      </Eq>
+   </Where>
+</Query></View>", ((FieldLookupValue)consumptionLI["Rate"]).LookupId);
+                    var rateLst = cc.Web.Lists.GetByTitle("Rates");
+                    var rateLIC = rateLst.GetItems(rateItemQuery);
+                    cc.Load(rateLIC);
+                    cc.ExecuteQuery();
+                    var orgItem = orgLIC.First();
+                    var rateItem = rateLIC.First();
+
+                    // create new charges item
+                    ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                    var newChgItem = chgLst.AddItem(itemCreateInfo);
+                    newChgItem["Organization"] = orgItem["Title"];
+                    newChgItem["Title"] = consumptionLI["Title"];
+                    newChgItem["Cycle"] = consumptionLI["Cycle"];
+                    newChgItem["Unit_x0020_Price"] = rateItem["Unit_x0020_Price"];
+                    newChgItem["Denominator"] = rateItem["Denominator"];
+                    newChgItem["UOM"] = rateItem["UOM"];
+                    newChgItem["Quantity"] = consumptionLI["Quantity"];
+                    FieldLookupValue lookup = new FieldLookupValue();
+                    lookup.LookupId = (int)consumptionLI["ID"];
+                    newChgItem["Consumption_x0020_Ref"] = lookup;
+
+                    foreach (KeyValuePair<string, List<string>> listColumnToCopy in listColumnsToCopy)
+                    {
+                        if (listColumnToCopy.Value.Count <= 0)
+                        {
+                            continue;
+                        }
+                        ListItem item = null;
+                        switch (listColumnToCopy.Key)
+                        {
+                            case "Rate":
+                                item = rateItem;
+                                break;
+                            case "Organization":
+                                item = orgItem;
+                                break;
+                        }
+                        foreach (var columnNm in listColumnToCopy.Value)
+                        {
+                            try
+                            {
+                                newChgItem[columnNm] = item[columnNm];
+                            }
+                            catch (Exception)
+                            {
+                                EventLog.WriteEntry(evtLogSrc, string.Format(@"Cannot copy column {0} in list {1}", columnNm, listColumnToCopy.Key), EventLogEntryType.Error);
+                            }
+                        }
+
+                    }
+
+                    if (consumptionLI["Amount"] != null)
+                        newChgItem["Amount"] = consumptionLI["Amount"];
+                    else if (consumptionLI["Quantity"] != null
+                        && rateItem["Denominator"] != null
+                        && rateItem["Unit_x0020_Price"] != null
+                        && rateItem["Denominator"] != null
+                        && (double)rateItem["Denominator"] > 0)
+                        newChgItem["Amount"] = (double)rateItem["Unit_x0020_Price"] * Math.Ceiling((double)consumptionLI["Quantity"] / (double)rateItem["Denominator"]);
+                    if (newChgItem.FieldValues.ContainsKey("Amount"))
+                    {
+                        newChgItem.Update();
+                        cc.ExecuteQuery();
+                    }
+                    else
+                    {
+                        EventLog.WriteEntry(evtLogSrc, "Cannot calculate amount for consumption item #" + consumptionLI["Id"], EventLogEntryType.Error);
+                    }
+
                     if (!consumptionLI.HasUniqueRoleAssignments)
                     {
                         consumptionLI.BreakRoleInheritance(true, false);
@@ -169,6 +205,31 @@ namespace Invoice_Run
                     cc.ExecuteQuery();
                 }
 
+                var chargesLst = cc.Web.Lists.GetByTitle("Charges");
+                var chargesLIC = chargesLst.GetItems(query);
+                cc.Load(chargesLIC, items => items.Include(
+                    item => item["Organization"]
+                    , item => item["HasUniqueRoleAssignments"]
+                    ));
+                cc.ExecuteQuery();
+                foreach (var chargeLI in chargesLIC)
+                {
+                    if (!chargeLI.HasUniqueRoleAssignments)
+                    {
+                        chargeLI.BreakRoleInheritance(true, false);
+                    }
+                    foreach (var g in gc)
+                    {
+                        if (g.LoginName == (groupPrefix + chargeLI["Organization"].ToString()))
+                        {
+                            var rdb = new RoleDefinitionBindingCollection(cc);
+                            rdb.Add(restReadRD);
+                            chargeLI.RoleAssignments.Add(g, rdb);
+                            break;
+                        }
+                    }
+                    cc.ExecuteQuery();
+                }
             }
             catch (Exception ex)
             {
